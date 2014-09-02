a45d3a1a45f7bc5fa14101e6bbb790f70276f8c3
ondrej <ondrejh.ck@email.cz>
2014-09-02 11:54:53 +0200

    new project file

---
+++ fw/blink/main.c
@@ -0,0 +1,30 @@
+/*
+ */
+
+#define F_CPU 3686400UL/8  // 3.68 MHz div 8 (460.8 kHz)
+
+#include <avr/io.h>
+#include <inttypes.h>
+#include <util/delay.h>
+
+#define LED_INIT() do{PORTC&=0x02;DDRC|=0x02;}while(0)
+#define LED_ON() do{PORTC|=0x02;}while(0)
+#define LED_OFF() do{PORTC&=~0x02;}while(0)
+#define LED_SWAP() do{PORTC^=0x02;}while(0)
+
+#define TXEN_INIT() do{PORTD&=0x04;DDRD|=0x04;}while(0)
+
+int main(void)
+{
+    // init port
+    LED_INIT();
+    TXEN_INIT();
+
+    while (1) //loop
+    {
+        LED_SWAP();
+        _delay_ms(500);
+    }
+
+    return 0;
+}
